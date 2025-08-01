f111f2143aaa80b4b704545450f31d21044bdca6
Chu A. <chu.amk22@gmail.com>
2025-08-01 13:42:06 +0100

    add: working with for loop

--- 02-logic-and-control-flow/script.js
+++ 02-logic-and-control-flow/script.js
@@ -1,9 +1,19 @@
 "use strict";
 //  The while loop
 
+console.log(`Using the WHILE LOOP`);
 let i = 0;
 
 while (i <= 10) {
   console.log(`Iteration number: ${i}`);
   i++;
+}
+
+console.log(``);
+console.log(`Using FOR LOOP`);
+
+// The for loop
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`Iteration number: ${i}`);
 }
