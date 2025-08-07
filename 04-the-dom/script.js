cde8e65fe97e16c123e94c5a748fd41934cc3abd
Chu A. <chu.amk22@gmail.com>
2025-08-07 10:20:47 +0100

    add: studied how to select HTML elements using the DOM

--- 04-the-dom/script.js
+++ 04-the-dom/script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const msg = document.getElementById("message")
+console.log(msg);
+
+const btn = document.getElementsByName("language")
+console.log(btn);
+
+const heading = document.getElementsByTagName("h1")
+console.log(heading);
+
+const title = document.getElementsByClassName("title")
+console.log(title);
