9e01e2f690a419a7c1e00b0a843fbc8247146064
Chu A. <chu.amk22@gmail.com>
2025-08-27 11:37:57 +0100

    worked with createElement() DOM

--- 04-the-dom/script.js
+++ 04-the-dom/script.js
@@ -11,3 +11,13 @@ console.log(heading);
 
 const title = document.getElementsByClassName("title")
 console.log(title);
+
+
+// Manipulating HTML Elements
+
+let div = document.createElement('div')
+div.innerHTML = "<h1>Hola amigo</h1>"
+
+document.body.appendChild(div)
+
+console.log(div);
