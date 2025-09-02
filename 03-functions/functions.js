5cd6d3bb20b374e1fa40994fc8537d599fede861
Chu A. <chu.amk22@gmail.com>
2025-09-02 13:26:43 +0100

    defining and calling a function

---
+++ 03-functions/functions.js
@@ -0,0 +1,13 @@
+function studentList() {
+  // fetch students
+
+  return [
+    {name: 'Alice', age: 20, grade: 'A'},
+    {name: 'Bod', age: 22, grade: 'B'},
+    {name: 'Charlie', age: 23, grade: 'C'},
+    {name: 'David', age: 21, grade: 'B'},
+    {name: 'Eve', age: 20, grade: 'A'}
+  ]
+}
+
+console.log(studentList());
