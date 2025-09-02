ace9bf869ba570ea9149639b0a4be74fb3e0e55a
Chu A. <chu.amk22@gmail.com>
2025-09-02 11:22:46 +0100

    working with multidimensional arrays

---
+++ 01-variables-datatypes/multidimensionalarrays.js
@@ -0,0 +1,13 @@
+let students = [
+  ['emmanuel', 30],
+  ['leslie', 20],
+  ['vanessa', 24],
+  ['nyap', 21],
+  ['Leila', 20],
+  ['arnold', 24]
+]
+
+// console.log(students[0][1]);
+students.push(['vincent', 22])
+students[2].push(1.68)
+console.log(students);
