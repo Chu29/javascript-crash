6a61d23b21d0900647d02fb898268db617ff39b0
Chu A. <chu.amk22@gmail.com>
2025-09-02 10:57:57 +0100

    working with arrays

---
+++ 01-variables-datatypes/arrays.js
@@ -0,0 +1,29 @@
+//defining arrays
+
+// let words = ['I', 'am', 'learning', 'JS', 'with','Vanessa']
+// let words1 = new Array('I', 'am', 'learning', 'JS', 'with','Vanessa')
+// console.log(words);
+
+// let numbers = new Array(10, 20, 30, 40)
+// console.log(numbers);
+
+let mixed = [10, 'Chu', true, null, undefined, {name: 'Chu'}, [10, 20, 30]]
+// console.log(mixed[0]);
+// console.log(mixed[1]);
+// console.log(mixed[2])
+// console.log(mixed[3]);
+// console.log(mixed.length);
+// console.log(mixed[mixed.length - 2]);
+console.log(mixed);
+
+mixed[2] = false
+
+mixed.push(10)
+// console.log(mixed[6][1]);
+// console.log(mixed[5].name);
+console.log(mixed);
+mixed.pop()
+console.log(mixed);
+
+// let emptyArr = []
+// console.log(emptyArr);
