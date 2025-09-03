fe73e04042788003257eff4877b2ff7cd3832b12
Chu A. <chu.amk22@gmail.com>
2025-09-03 13:05:24 +0100

    conditional statements

--- 02-logic-and-control-flow/controlFlow.js
+++ 02-logic-and-control-flow/controlFlow.js
@@ -1,2 +1,15 @@
 // control flow statements
 
+let prompt = require('prompt-sync')();
+
+const input = prompt('Enter a number: ')
+if(input > 0) {
+  console.log('Positive');
+} else {
+  console.log('Non-positive');
+  if (input < 0) {
+    console.log('Negative');
+  } else {
+    console.log('Zero');
+  }
+}
