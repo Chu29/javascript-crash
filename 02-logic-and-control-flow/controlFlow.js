0515d097a7cdbfa0b83c25b1cb806eb1f07ab11f
Chu A. <chu.amk22@gmail.com>
2025-09-03 13:16:44 +0100

    switch statements

--- 02-logic-and-control-flow/controlFlow.js
+++ 02-logic-and-control-flow/controlFlow.js
@@ -13,3 +13,26 @@ if(input > 0) {
     console.log('Zero');
   }
 }
+
+// switch statements
+
+switch (input) {
+  case '1':
+    console.log("one");
+    break;
+  case '2':
+    console.log("two");
+    break;
+   case '3':
+    console.log("three");
+    break;
+  case '-1':
+  case '-2':
+  case '-3':
+  case '-4':
+    console.log("negative numbers");
+    break;
+  default:
+    console.log("other number");
+    break;
+}
