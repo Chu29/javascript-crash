bd3dc0a6df96a842f7af107fe1d37934481c12b1
Chu A. <chu.amk22@gmail.com>
2025-09-16 07:13:07 +0100

    contact list app using arrays

---
+++ exercises/04-objects-and-arrays/contactList.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const contactList = [
+  {
+    firstName: "Blessing",
+    lastName: "Wai",
+    phone: +237650504322,
+    location: "Limbe",
+  },
+  {
+    firstName: "Bannah",
+    lastName: "Ryan",
+    phone: +237650504322,
+    location: "Bamenda",
+  },
+  {
+    firstName: "Munda",
+    lastName: "Bladini",
+    phone: +237650504322,
+    location: "Bamenda",
+  },
+  {
+    firstName: "Penn",
+    lastName: "Nkam",
+    phone: +237650504322,
+    location: "Bambili",
+  },
+  {
+    firstName: "Ayugho",
+    lastName: "Olson",
+    phone: +237650504322,
+    location: "Bamenda",
+  },
+];
+
+for (const element of contactList) {
+  console.log(element);
+}
