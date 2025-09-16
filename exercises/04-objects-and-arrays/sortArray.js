774aeb8b4ef2b2d700a101383a938569b2bf80c5
Chu A. <chu.amk22@gmail.com>
2025-09-16 07:14:27 +0100

    refactored the code

--- exercises/04-objects-and-arrays/sortArray.js
+++ exercises/04-objects-and-arrays/sortArray.js
@@ -1,26 +1,31 @@
 "use strict";
 const student = [
   {
-    id: 0,
+    id: 1,
     name: "Abuemkeze",
     age: 22,
   },
   {
-    id: 1,
+    id: 0,
     name: "Dave",
     age: 23,
   },
   {
-    id: 2,
+    id: 3,
     name: "Vincent",
     age: 21,
   },
   {
-    id: 3,
+    id: 2,
     name: "Emma",
     age: 47,
   },
+  {
+    id: 4,
+    name: "Blessing",
+    age: 19,
+  },
 ];
 
-const sorted = student.sort((asc, desc) => asc.age - desc.age);
-console.log(sorted);
+const sorted = student.sort((asc, desc) => asc.name.localeCompare(desc.name));
+console.log("Sorted Array: => ", sorted);
