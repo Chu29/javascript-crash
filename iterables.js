c5dd96a4ce2d13c743eab05582b45eb6c2c48aff
Chu A. <chu.amk22@gmail.com>
2025-09-21 08:09:42 +0100

    iterators

---
+++ iterables.js
@@ -0,0 +1,56 @@
+// array iterable
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (const element of arr) {
+  console.log(element);
+}
+// get the iterator object for the array
+const arrIterator = arr[Symbol.iterator]();
+console.log(arrIterator);
+
+// string iterable
+const string = "String";
+
+for (const element of string) {
+  console.log(element);
+}
+// get the iterator object for the string
+const strIterator = string[Symbol.iterator]();
+console.log(strIterator);
+
+console.log("Symbol.iterator: ", Symbol.iterator);
+
+// using the next() method to check if the iterator is ended.
+console.log(strIterator.next());
+console.log(strIterator.next());
+console.log(strIterator.next());
+console.log(strIterator.next());
+console.log(strIterator.next());
+console.log(strIterator.next());
+console.log(strIterator.next());
+
+console.log("\n\n");
+
+function displayEvent(arr) {
+  let i = 0;
+  return {
+    next() {
+      if (i < arr.length && arr[i] % 2 !== 0) {
+        i++;
+      }
+      return {
+        value: i >= arr.length ? undefined : arr[i],
+        done: i++ >= arr.length ? true : false,
+      };
+    },
+  };
+}
+
+const eveIterator = displayEvent(arr);
+
+console.log(eveIterator.next());
+console.log(eveIterator.next());
+console.log(eveIterator.next());
+console.log(eveIterator.next());
+console.log(eveIterator.next());
+console.log(eveIterator.next());
