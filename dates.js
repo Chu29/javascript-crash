8b7f053f3386a5840f0dfffb6cbb0f648daaf248
Chu A. <chu.amk22@gmail.com>
2025-09-21 08:08:04 +0100

    the dat object

---
+++ dates.js
@@ -0,0 +1,18 @@
+// const now = new Date();
+// console.log(now, typeof now);
+
+// console.log(now.toString());
+
+const someDate = new Date();
+console.log(someDate.toString());
+console.log(someDate.getFullYear());
+console.log(someDate.getMonth());
+console.log(someDate.getDate());
+console.log(someDate.getHours());
+console.log(someDate.getMinutes());
+console.log(someDate.getSeconds());
+console.log(someDate.getMilliseconds());
+console.log(someDate.getDay());
+
+// const dateNow = Date.now();
+// console.log(dateNow);
